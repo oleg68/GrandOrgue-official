a9559f237bb3f298f6d05ea7b87f99da5069516c
Oleg Samarin <osamarin@openintegration.inc>
2026-02-09 23:15:14 +0300

    Added GoTestExe distribution

--- build-scripts/for-linux/build-for-tests.sh
+++ build-scripts/for-linux/build-for-tests.sh
@@ -26,3 +26,7 @@ echo "cmake -G \"Unix Makefiles\" $GO_PRMS . $SRC_DIR"
 cmake -G "Unix Makefiles" $GO_PRMS . $SRC_DIR
 echo "cmake --build . --parallel $PARALLEL_PRMS"
 cmake --build . --parallel $PARALLEL_PRMS
+
+# Create tar.gz package for easy distribution
+echo "Creating test package with cpack"
+cpack -G TGZ
