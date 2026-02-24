d2f9c5361f3ab5a45663be343bee3bdeb768ff32
Oleg Samarin <osamarin@openintegration.inc>
2026-02-24 20:12:20 +0300

    Restoring waiting for threads

--- src/grandorgue/sound/GOSoundSystem.cpp
+++ src/grandorgue/sound/GOSoundSystem.cpp
@@ -176,6 +176,7 @@ void GOSoundSystem::BuildAndStartEngine() {
     m_OrganController->GetMemoryPool(),
     m_config.ReleaseConcurrency());
   StartThreads();
+  m_SoundEngine.GetScheduler().ResumeGivingWork();
 }
 
 void GOSoundSystem::StartSoundSystem() {
@@ -210,6 +211,9 @@ void GOSoundSystem::StopAndDestroyEngine() {
   for (GOSoundOutput &output : m_AudioOutputs)
     multi.Add(output.mutex);
 
+  m_SoundEngine.GetScheduler().PauseGivingWork();
+  for (GOSoundThread *pThread : m_Threads)
+    pThread->WaitForIdle();
   StopThreads();
   m_SoundEngine.ClearSetup();
 }
