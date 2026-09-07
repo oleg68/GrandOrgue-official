GrandOrgue/grandorgue

Switched sound tasks to GOSoundTaskBase
Each of the seven GOSoundTask implementations (GOSoundTremulantTask,
GOSoundWindchestTask, GOSoundTouchTask, GOSoundOutputTask,
GOSoundGroupTask, GOSoundReleaseTask, GOSoundRecorder) independently
duplicated the same priority/cost/repeatable constants, its own mutex
and "done" flag, and the same double-checked-lock preamble in Run().
GOSoundTaskBase now owns that round life cycle; each task keeps only
what is genuinely its own: DoRun() for the regular one-shot protocol,
or a full Run()/CompleteRound() override for the two tasks with a
different execution protocol (GOSoundGroupTask's cooperative merge,
GOSoundReleaseTask's drain loop).

Co-Authored-By: Claude Sonnet 5 <noreply@anthropic.com>
Claude-Session: https://claude.ai/code/session_018s9Czsi7RrUUzr9VvTGv9P

Added the gate measuring the cooperative round's entry latency

Replacing GOSoundGroupTask's blocking entry section with a lock-free one
was to be accepted or rejected by comparing perf runs before and after,
but no scenario measured the thing it changes. RoundProtocolCost runs on
the base task, whose mutex is deliberately kept; CooperativeThroughput is
aggregate throughput, in which the cost of entering a round disappears.
Both would have moved barely at all whatever the rewrite did - the same
blind spot that made the first version of these tests unusable as a gate.

CooperativeRoundLatency takes the NextPeriodLatency shape - one thread
standing in for the audio thread timing CompleteRound() + NewRound()
while workers race Run() - and points it at the cooperative task. Each
thread's share is a 200 item quota, a few hundred nanoseconds, so what is
timed is the entry and merge sections rather than the work between them.
The deadline is called with no delay: letting the round run first only
means the workers finish it before the deadline arrives, and the
measurement degenerates to CompleteRound() returning at its own
RUN_STATE_DONE guard, having contended for nothing. Me

src/grandorgue/sound/tasks/GOSoundWindchestTask.cpp
@@ -8,15 +8,14 @@
 #include "GOSoundWindchestTask.h"
 
 #include "sound/GOSoundOrganEngine.h"
-#include "threading/GOMutexLocker.h"
 
 #include "GOSoundTremulantTask.h"
 
 GOSoundWindchestTask::GOSoundWindchestTask(
   GOSoundOrganEngine &soundEngine, GOWindchest *pWindchest)
-  : r_engine(soundEngine),
+  : GOSoundTaskBase(PRIORITY_WINDCHEST, false),
+    r_engine(soundEngine),
     m_volume(0),
-    m_done(false),
     p_windchest(pWindchest) {}
 
 void GOSoundWindchestTask::Init(
@@ -28,26 +27,15 @@ void GOSoundWindchestTask::Init(
         tremulantTasks[p_windchest->GetTremulantId(i)]);
 }
 
-void GOSoundWindchestTask::NewRound() {
-  GOMutexLocker locker(m_mutex);
+bool GOSoundWindchestTask::DoRun(GOSchedulerThread *pThread) {
+  float volume = r_engine.GetGain();
 
-  m_done.store(false);
-}
-
-void GOSoundWindchestTask::Run(GOSchedulerThread *pThread) {
-  if (!m_done.load()) {
-    GOMutexLocker locker(m_mutex);
-
-    if (!m_done.load()) {
-      float volume = r_engine.GetGain();
-
-      if (p_windchest) {
-        volume *= p_windchest->GetVolume();
-        for (unsigned i = 0; i < m_pTremulantTasks.size(); i++)
-          volume *= m_pTremulantTasks[i]->GetVolume();
-      }
-      m_volume = volume;
-      m_done.store(true);
-    }
+  if (p_windchest) {
+    volume *= p_windchest->GetVolume();
+    for (unsigned i = 0; i < m_pTremulantTasks.size(); i++)
+      volume *= m_pTremulantTasks[i]->GetVolume();
   }
+  m_volume = volume;
+
+  return true;
 }

oleg68/GrandOrgue-official

src/grandorgue/sound/tasks/GOSoundWindchestTask.cpp
@@ -8,15 +8,14 @@
 #include "GOSoundWindchestTask.h"
 
 #include "sound/GOSoundOrganEngine.h"
-#include "threading/GOMutexLocker.h"
 
 #include "GOSoundTremulantTask.h"
 
 GOSoundWindchestTask::GOSoundWindchestTask(
   GOSoundOrganEngine &soundEngine, GOWindchest *pWindchest)
-  : r_engine(soundEngine),
+  : GOSoundTaskBase(PRIORITY_WINDCHEST, false),
+    r_engine(soundEngine),
     m_amplitude(0),
-    m_done(false),
     p_windchest(pWindchest) {}
 
 void GOSoundWindchestTask::Init(
@@ -28,26 +27,15 @@ void GOSoundWindchestTask::Init(
         tremulantTasks[p_windchest->GetTremulantId(i)]);
 }
 
-void GOSoundWindchestTask::NewRound() {
-  GOMutexLocker locker(m_mutex);
+bool GOSoundWindchestTask::DoRun(GOSchedulerThread *pThread) {
+  float amplitude = r_engine.GetAmplitude();
 
-  m_done.store(false);
-}
-
-void GOSoundWindchestTask::Run(GOSchedulerThread *pThread) {
-  if (!m_done.load()) {
-    GOMutexLocker locker(m_mutex);
-
-    if (!m_done.load()) {
-      float amplitude = r_engine.GetAmplitude();
-
-      if (p_windchest) {
-        amplitude *= p_windchest->GetAmplitude();
-        for (unsigned i = 0; i < m_pTremulantTasks.size(); i++)
-          amplitude *= m_pTremulantTasks[i]->GetAmplitude();
-      }
-      m_amplitude = amplitude;
-      m_done.store(true);
-    }
+  if (p_windchest) {
+    amplitude *= p_windchest->GetAmplitude();
+    for (unsigned i = 0; i < m_pTremulantTasks.size(); i++)
+      amplitude *= m_pTremulantTasks[i]->GetAmplitude();
   }
+  m_amplitude = amplitude;
+
+  return true;
 }